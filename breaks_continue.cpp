f3bb077527203f8ff4212190a3a5c6be6781d24a
Yash <salunkeyaash12@gmail.com>
2023-07-07 12:24:53 +0530

    Lecture-11

---
+++ breaks_continue.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // Break Statement
+
+    // for(int i=0; i<5; i++){
+    //     if(i==2){
+    //         break;
+    //     }
+    //     cout << i << endl;
+    // }
+
+    for(int i=0; i<5; i++){
+        if(i==3){
+            continue;
+        }
+        cout << i << endl;
+    }
+
+    return 0;
+}
